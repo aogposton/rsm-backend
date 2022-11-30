df7b4c9458e5dfb63831552c580b62af9e90247a
immaculateintellect <amon@immaculateintellect.com>
2022-11-29 23:31:46 -0500

    Initial commit

---
+++ database/migrations/2022_11_30_033206_create_breweries_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('breweries', function (Blueprint $table) {
+            $table->string('id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('brewery_type')->nullable();
+            $table->string('street')->nullable();
+            $table->string('address_2')->nullable();
+            $table->string('address_3')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('county_province')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->string('country')->nullable();
+            $table->string('longitude')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('website_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('breweries');
+    }
+};
